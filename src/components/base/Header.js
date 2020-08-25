a600d6515ce0b87b172b209e94c5d08e450717ac
Dmitriy <knutdmit@yandex.ru>
2020-08-25 13:10:03 +0500

    fix

--- src/components/base/Header.js
+++ src/components/base/Header.js
@@ -1,31 +1,8 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useRouteMatch,
-  useParams
-} from "react-router-dom";
 
 export default function Header() {
   return (
     <div>
-      <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/topics">Topics</Link>
-            </li>
-          </ul>
-        </div>
-      </Router>
       <h1>Header</h1>
     </div>
   );
